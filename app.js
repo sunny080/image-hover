1d8f359798264fc6f65ec69e447795afeca4f880
Sunny <77973332+sunny080@users.noreply.github.com>
2021-11-23 00:59:40 -0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,42 @@
+new hoverEffect({
+  parent:document.querySelector('.distortion'),
+  intensity:0.2,
+  image1:"fip.jpg",
+  image2:"fklp.jpg",
+  angle:Math.PI/8,
+  displacementImage:"heightMap.png"
+});
+
+new hoverEffect({
+    parent:document.querySelector('.robot'),
+    intensity:0.2,
+    image1:"dido.jpg",
+    image2:"dolp.jpg",
+    angle:Math.PI/8,
+    displacementImage:"fluid.jpg"
+  });
+
+  new hoverEffect({
+    parent:document.querySelector('.dalone'),
+    intensity:0.2,
+    image1:"eltip.png",
+    image2:"fip.jpg",
+    angle:Math.PI/8,
+    displacementImage:"pilat.jpg"
+  });
+  new hoverEffect({
+    parent:document.querySelector('.alone'),
+    intensity:0.2,
+    image1:"gimk.jpg",
+    image2:"gimy.jpg",
+    angle:Math.PI/8,
+    displacementImage:"ice.jpg"
+  });
+  new hoverEffect({
+    parent:document.querySelector('.bully'),
+    intensity:0.2,
+    image1:"jiographi.png",
+    image2:"retrac.png",
+    angle:Math.PI/8,
+    displacementImage:"dot.jpg"
+  });
